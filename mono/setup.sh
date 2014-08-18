723e71307877efb634e9e84d1b5d78e1e564cfa9
Brandon Holt <bholt@cs.washington.edu>
2014-08-17 21:50:13 -0700

    change prompt

--- mono/setup.sh
+++ mono/setup.sh
@@ -51,7 +51,7 @@ cd /
 echo "sudo sysctl -w kernel.shmmax=$((1<<30)) >/dev/null 2>/dev/null" >> /etc/profile
 
 # set prompt colors because I'm a bit vain about that
-echo "export PS1='\[\e[0;34m\]docker \[\e[m\]\[\e[0;32m\]\w\[\e[m\] \[\e[0;33m\]\$ \[\e[m\]'" >> /etc/profile
+echo "export PS1='\[\e[0;34m\]docker \[\e[m\]\[\e[0;32m\]\w\[\e[m\] \[\e[0;33m\]> \[\e[m\]'" >> /etc/profile
 
 # enable tab completion
 echo "source /etc/bash_completion" >> /etc/profile
