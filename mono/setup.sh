ece50c773189628a7f3af34f9911296b5d018f52
Brandon Holt <bholt@cs.washington.edu>
2014-08-19 00:00:15 -0700

    add openssl

--- mono/setup.sh
+++ mono/setup.sh
@@ -10,7 +10,8 @@ apt-get install -y \
   git \
   openssh-server \
   bash-completion \
-  man
+  man \
+  openssl
 
 # build recent version of Ruby
 wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz && \
