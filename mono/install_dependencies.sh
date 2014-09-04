e8cb621dcfb01fc9ef06cd2c5851355069750e54
Brandon Holt <bholt@cs.washington.edu>
2014-09-04 17:35:00 -0500

    split dependency building and environment setup

--- mono/install_dependencies.sh
+++ mono/install_dependencies.sh
@@ -47,12 +47,3 @@ cd grappa
   ninja
 
 cd /
-
-# have to make sure we re-set shmmax before running any Grappa programs
-echo "sudo sysctl -w kernel.shmmax=$((1<<30)) >/dev/null 2>/dev/null" >> /etc/profile
-
-# set prompt colors because I'm a bit vain about that
-echo "export PS1='\[\e[0;34m\]docker \[\e[m\]\[\e[0;32m\]\w\[\e[m\] \[\e[0;33m\]> \[\e[m\]'" >> /etc/profile
-
-# enable tab completion
-echo "source /etc/bash_completion" >> /etc/profile
